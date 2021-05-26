114dd3571d33a4b52222b90c3f2cb916dc4f4e62
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-05-26 16:40:07 +0530

    major working commit

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const loadImage = (event) => {
   const image = document.getElementById("imgDisplayed");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
-document.getElementById("participant").onclick = function changeContent() {
+
+
+
+document.getElementById("member").onclick = function changeContent() {
   document.getElementById("specific").style.display = "block";
   document.getElementById("chooseOption").style.display = "none";
   document.getElementById("sectionHeading").innerHTML =
@@ -16,11 +19,11 @@ document.getElementById("participant").onclick = function changeContent() {
     canvas.width = 600;
     console.log(canvas.height, canvas.width);
     const ctx = canvas.getContext("2d");
-    ctx.rect(0, 0, 1060, 600);
+    ctx.rect(0, 0, 960, 0);
     ctx.fill();
 
     const image = document.getElementById("imgDisplayed");
-    ctx.drawImage(image, 50, 20, 500, 480);
+    ctx.drawImage(image, 30, 20, 545, 530);
     const badgeDesign = document.getElementById("badgeDesign1");
     ctx.drawImage(badgeDesign, 0, 0, 600, 600);
   };
@@ -38,12 +41,17 @@ document.getElementById("participant").onclick = function changeContent() {
     const a = document.createElement("a");
     document.body.appendChild(a);
     a.href = canvasContent;
-    a.download = "ParticipantBadge.png";
+    a.download = "MemberBadge.png";
     a.click();
     document.body.removeChild(a);
   });
 };
-document.getElementById("mentor").onclick = function changeContent() {
+
+
+
+
+
+document.getElementById("champion").onclick = function changeContent() {
   document.getElementById("specific").style.display = "block";
   document.getElementById("chooseOption").style.display = "none";
   document.getElementById("sectionHeading").innerHTML =
@@ -57,11 +65,11 @@ document.getElementById("mentor").onclick = function changeContent() {
     canvas.width = 600;
     console.log(canvas.height, canvas.width);
     const ctx = canvas.getContext("2d");
-    ctx.rect(0, 0, 1060, 600);
+    ctx.rect(0, 0, 960, 0);
     ctx.fill();
 
     const image = document.getElementById("imgDisplayed");
-    ctx.drawImage(image, 50, 20, 500, 480);
+    ctx.drawImage(image, 30, 20, 545, 530);
 
     const badgeDesign = document.getElementById("badgeDesign2");
     ctx.drawImage(badgeDesign, 0, 0, 600, 600);
@@ -80,7 +88,50 @@ document.getElementById("mentor").onclick = function changeContent() {
     const a = document.createElement("a");
     document.body.appendChild(a);
     a.href = canvasContent;
-    a.download = "MentorBadge.png";
+    a.download = "ChampionBadge.png";
+    a.click();
+    document.body.removeChild(a);
+  });
+};
+
+
+document.getElementById("winner").onclick = function changeContent() {
+  document.getElementById("specific").style.display = "block";
+  document.getElementById("chooseOption").style.display = "none";
+  document.getElementById("sectionHeading").innerHTML =
+    "Hey, Winner Please download your badge from below !";
+  document.getElementById("badge-container").style.display = "block";
+
+  const preview = document.getElementById("preview");
+  const prev = () => {
+    const canvas = document.getElementById("result");
+    canvas.height = 600;
+    canvas.width = 600;
+    console.log(canvas.height, canvas.width);
+    const ctx = canvas.getContext("2d");
+    ctx.rect(0, 0, 960, 0);
+    ctx.fill();
+
+    const image = document.getElementById("imgDisplayed");
+    ctx.drawImage(image, 30, 20, 545, 530);
+    const badgeDesign = document.getElementById("badgeDesign3");
+    ctx.drawImage(badgeDesign, 0, 0, 600, 600);
+  };
+  preview.addEventListener("click", prev);
+
+  // const loadImage = (event) => {
+  //   const image = document.getElementById("imgDisplayed");
+  //   image.src = URL.createObjectURL(event.target.files[0]);
+  // };
+
+  const download = document.getElementById("download");
+  download.addEventListener("click", () => {
+    const canvas = document.getElementById("result");
+    const canvasContent = canvas.toDataURL();
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = canvasContent;
+    a.download = "WinnerBadge.png";
     a.click();
     document.body.removeChild(a);
   });
